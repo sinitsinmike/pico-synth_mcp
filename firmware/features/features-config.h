3250b5edd15a50d773175258d50c20422b244d36
sinitsinmike <sinitsinmike@yahoo.com>
2025-10-28 12:49:07 +0300

    Add files via upload

--- firmware/features/features-config.h
+++ firmware/features/features-config.h
@@ -1,3 +1,4 @@
 #pragma once
 /* Comment out a line to disable that feature entirely. */
-#define PS_FEATURES(X) /*  X(feature_midi_monitor) */
+#define PS_FEATURES(X) 
+/*  X(feature_midi_monitor) */
